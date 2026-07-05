1c114bda01bee8981c9ebb2c7c737247bd0dab62
shafranovskijm-pixel <shafranovskij.m@gmail.com>
2026-07-04 21:44:01 -0700

    feat: configure Capacitor Android debug build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.lovable.d57ddcdf2d1b42ec8bfb3484123b5ff2',
   appName: 'sintagma',
-  webDir: 'dist'
+  webDir: 'dist',
+  : android: {
+    buildOptions: {
+      releaseType: 'APK'
+    }
+  }
+
 };
 
 export default config;
